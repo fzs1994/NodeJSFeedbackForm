1c2da72317dcf39b7700b9b46e2e71faad8100b5
Faizan Saiyed <fsaiyed@officebrain.com>
2017-05-17 09:31:13 +0530

    Authentication using Passport in NodeJS

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://objsonresume:ob1jsonresume@ds139761.mlab.com:39761/feedbackform' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
